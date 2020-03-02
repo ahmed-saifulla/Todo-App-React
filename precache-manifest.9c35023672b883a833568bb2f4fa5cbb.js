6325835aeb467ce30cca2531599a45dfd74c2512
ahmed-saifulla <ahsaifulla@gmail.com>
2020-03-02 16:45:54 +0530

    Updates

---
+++ precache-manifest.9c35023672b883a833568bb2f4fa5cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2892b0f1b0b832453d20303d201d2325",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ee70844334621980ba12",
+    "url": "/todo-react/static/css/main.11ba98f5.chunk.css"
+  },
+  {
+    "revision": "8409b56a8d4e1e69ef65",
+    "url": "/todo-react/static/js/2.d41ec44d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react/static/js/2.d41ec44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee70844334621980ba12",
+    "url": "/todo-react/static/js/main.06eaf051.chunk.js"
+  },
+  {
+    "revision": "91e8e24e5bebb6591cda",
+    "url": "/todo-react/static/js/runtime-main.18ee543e.js"
+  }
+]);
