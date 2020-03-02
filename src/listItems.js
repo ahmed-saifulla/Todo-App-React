12cf0f283df37c4428453c0e8e7ffb3f7ef03572
ahmed-saifulla <ahsaifulla@gmail.com>
2020-03-02 16:37:31 +0530

    flip-move animation

--- src/listItems.js
+++ src/listItems.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './ListItems.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 
 
 function ListItems(props) {
     const items = props.items;
     const listItems = items.map(item => {
         return <div className="list-item" key={item.key}>
-            <input type="text" value={item.text} onChange={ (e) => {props.updateItem(e.target.value, item.key)} } />  
-            <span>  <FontAwesomeIcon className="faicons" icon="trash" onClick={ () => props.deleteItem(item.key)} /> </span>
+            <input type="text" value={item.text} onChange={(e) => { props.updateItem(e.target.value, item.key) }} />
+            <span>  <FontAwesomeIcon className="faicons" icon="trash" onClick={() => props.deleteItem(item.key)} /> </span>
         </div>
     })
     return (
-        <div>{listItems}</div>
+
+        <div>
+            <FlipMove duration={300} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+        </div>
     )
 }
 
